e5e52ad9d39654fbc8a8bc6db38641920e2bb063
Viviane Suzuki <vivisuzuki@hotmail.com>
2023-09-11 22:44:50 -0300

    Adicionando inputs e validações na tela de login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,9 +1,58 @@
-import { Button, SafeAreaView, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, SafeAreaView, Text, TextInput } from "react-native";
+
 
 const Login = ({ navigation }) => {
+
+    const [inputEmailValue, setInputEmailValue] = useState()
+    const [inputEmailValid, setInputEmailValid] = useState(true)
+
+    const [inputPasswordValue, setInputPasswordValue] = useState()
+    const [inputPasswordValid, setInputPasswordValid] = useState(true)
+
+    const onChangeEmailValue = (emailValue) => {
+
+        const emailRegex = /\S+@\S+\.\S+/;
+        let validacao = emailRegex.test(emailValue);
+
+        if (validacao) {
+            setInputEmailValid(true)
+        } else {
+            setInputEmailValid(false)
+        }
+        setInputEmailValue(emailValue)
+    }
+
+    const onChangePasswordValue = (passwordValue) => {
+
+        const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%^&*()_+])[A-Za-z\d@#$!%^&*()_+]{8,}$/; //regex para maiuscula, minuscula e caracter especial
+        let validacao = senhaRegex.test(passwordValue);
+
+        if (passwordValue.length >= 8 && validacao) {
+            setInputPasswordValid(true)
+        } else {
+            setInputPasswordValid(false)
+        }
+        setInputPasswordValue(passwordValue)
+    }
+
     return (
         <SafeAreaView style={ { marginTop: 20 } }>
-            <Text>Tela Login</Text>
+            <TextInput
+                value={ inputEmailValue }
+                onChangeText={ onChangeEmailValue }
+                onEndEditing={ () => setInputEmailValid(true) }
+                placeholder="Ex: example@teste.com"
+            />
+            <TextInput
+                value={ inputPasswordValue }
+                onChangeText={ onChangePasswordValue }
+                onBlur={ () => setInputPasswordValid(true) }
+                placeholder="Digite sua senha"
+            />
+            { inputEmailValid === true ? <></> : <Text>Email não é válido</Text> }
+            { inputPasswordValid === true ? <></> : <Text>Senha não é válida</Text> }
+
             <Button
                 title="Login"
                 onPress={ () => navigation.navigate('Home') }
